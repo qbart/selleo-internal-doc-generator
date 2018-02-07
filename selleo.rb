4d7e13f5db09866289ff1bdc2d80e49084f7b6dd
Bartlomiej Wojtowicz <b.wojtowicz@selleo.com>
2018-02-07 11:03:12 +0100

    init

---
+++ selleo.rb
@@ -0,0 +1,132 @@
+#!/bin/ruby
+
+require 'pry'
+require 'date'
+
+class XDate < Date
+  DAYS = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31].freeze
+
+  def begining_of_the_prev_month
+    prev_month
+  end
+
+  def end_of_the_prev_month
+    prev    = prev_month
+    new_day = DAYS[prev.month - 1]
+
+    if new_day == 28 && self.class.leap?(prev.year)
+      new_day = 29
+    end
+
+    XDate.new(prev.year, prev.month, new_day)
+  end
+
+  private
+
+  def prev_month
+    new_month = month - 1
+    new_year  = year
+
+    if (new_month == 0)
+      new_month = 12
+      new_year -= 1
+    end
+
+    XDate.new(new_year, new_month, 1)
+  end
+end
+
+class StdinReader
+  def gets
+    lines = []
+    loop do
+      line = STDIN.gets.chomp
+      break if line.empty?
+      lines << line
+    end
+    lines.join("\n")
+  end
+end
+
+class Pdf
+  def initialize(template)
+    @pwd      = Dir.pwd
+    @template = File.join(@pwd, template)
+  end
+
+  def generate(params)
+    replacements = {
+      "{{NET_INCOME}}" => params.net_income,
+      "{{BEGIN_DATE}}" => params.begin_date.to_s,
+      "{{END_DATE}}"   => params.end_date.to_s,
+      "{{LIST}}"       => params.todos.gsub("\n", "<br>").gsub(" ", "&nbsp;")
+    }
+    file_name = "#{params.begin_date.to_s.gsub("-", "")}-#{params.end_date.to_s.gsub("-", "")}.pdf"
+
+    Dir.mktmpdir do |dir|
+      unzip = "unzip \"#{File.join(@template)}\" -d #{dir}"
+
+      puts "Running: #{unzip}"
+      system unzip
+
+      html_file = Dir[File.join(dir, "*.html")].first
+      puts "Replacing params tmp file: #{html_file}"
+      data = File.read(html_file)
+      replacements.each do |key, value|
+        data.gsub!(key, value)
+      end
+      File.open(html_file, "w") do |f|
+        f.write(data)
+      end
+
+      puts "Copying pdf"
+      out_dir = File.join(@pwd, "pdf")
+      FileUtils.mkdir_p(out_dir)
+      pdf = "wkhtmltopdf \"#{html_file}\" \"#{File.join(dir, "out.pdf")}\""
+      system pdf
+      FileUtils.cp(File.join(dir, "out.pdf"), File.join(out_dir, file_name))
+    end
+  end
+end
+
+Params = Struct.new(:net_income, :begin_date, :end_date, :todos) do
+  def valid?
+    !net_income.nil?
+  end
+
+  def calculate_dates
+    self.begin_date = XDate.today.begining_of_the_prev_month
+    self.end_date   = XDate.today.end_of_the_prev_month
+  end
+
+  def to_s
+    <<~STR
+    NET_INCOME: #{net_income}zł
+    BEGIN_DATE: #{begin_date}
+    END_DATE:   #{end_date}
+    TODOs:
+      #{todos}
+    STR
+  end
+end
+
+args = ARGV.dup
+
+params = Params.new
+params.calculate_dates
+params.net_income = args.shift
+
+if params.valid?
+  puts "Provide TODOs, example [empty line will finish the prompt]:"
+  puts "- Project 1"
+  puts "  - module A"
+  puts "  - module B"
+  puts ""
+  puts "Prompt:\n"
+  params.todos = StdinReader.new.gets
+  puts "Generating PDF with:\n#{params}"
+  pdf = Pdf.new("protokol.zip")
+  pdf.generate(params)
+else
+  puts "Not enough params to generate PDF"
+end
